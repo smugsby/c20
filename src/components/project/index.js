548d15cc6a35abd776a81d107a6e81849c61dee4
smugsby <dhuffman87@gmail.com>
2022-09-15 18:23:06 -0500

    check it out

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -5,7 +5,7 @@ const Project = () => {
     return (
         <>
             <div className="bioContainer">
-                <img className='bioImg' src="assets\images\278186468_2514830141981792_8051652052965352497_n.jpg" alt="Shawn has a big head"></img>
+                <img className='bioImg' src="assets/images/week2.jpg" alt="Shawn has a big head"></img>
                 <div className="bioText">WRITE BIO HERE</div>
             </div>
             <div className="projectsContainer">
