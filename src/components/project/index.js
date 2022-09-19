4faa7f2acb271da98710ba9d631d3fca1b7820d9
smugsby <dhuffman87@gmail.com>
2022-09-19 15:33:29 -0500

    graded

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -5,27 +5,27 @@ const Project = () => {
     return (
         <>
             <div className="bioContainer">
-                <img className='bioImg' src="assets/images/week2.jpg" alt="Shawn has a big head"></img>
-                <div className="bioText">WRITE BIO HERE</div>
+                <img className='bioImg' src="assets/images/week2.jpg" alt="Huff thumb"></img>
+                <div className="bioText">Thank you for stopping by. This is my Portfolio... or at least the start of it.<br>Feel free to click some images below and see some of my really early projects.</br></div>
             </div>
             <div className="projectsContainer">
                 <div className='as1'>
                     <h5>landing page</h5>
-                    <a href = "https://smugsby.github.io/week1challenge/" target = "_blank" rel = "noreferrer"><img src='../Week2/assets/images/One Week Challenge.jpg' alt=''></img></a>
+                    <a href = "https://smugsby.github.io/week1challenge/" target = "_blank" rel = "noreferrer"><img src='assets/images/One Week Challenge.jpg' alt=''></img></a>
                 </div>
                 <div className='as1'>
                     <h5>landing page</h5>
-                    <img src='assets\images\templatephonesites.jpg' alt=''></img>
+                    <a href = "https://smugsby.github.io/week1challenge/" target = "_blank" rel = "noreferrer"><img src='assets/images/Robot Gladiator Fight Game.jpg' alt=''></img></a>
                 </div>
                 <div className='as1'>
                     <h5>landing page</h5>
-                    <img src='assets\images\templatephonesites.jpg' alt=''></img>
+                    <a href = "https://smugsby.github.io/week1challenge/" target = "_blank" rel = "noreferrer"><img src='assets/images/Smugsby.jpg' alt=''></img></a>
                 </div>
                 <div className='as1'>
                     <h5>landing page</h5>
-                    <img src='assets\images\templatephonesites.jpg' alt=''></img>
+                    <a href = "https://smugsby.github.io/week1challenge/" target = "_blank" rel = "noreferrer"><img src='assets/images/week2.jpg' alt=''></img></a>
                 </div>
-                
+                                
             </div>
         </>
     )
